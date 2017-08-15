3f709307d0ee60796ddfb332395a2f745f65f98d
dengzhw <314186006@qq.com>
2017-08-15 22:37:52 +0800

    add login

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import index
+from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
+    url(r'^postTest/$', postTest, name='postTest'),
+    url(r'^postTest1/$', postTest1, name='postTest1'),
+
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Max, F, Q
 from .models import BookInfo
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,6 +9,17 @@ def index(request):
     # list = BookInfo.books1.filter(bread__gte=F('bcomment')*0)
     # list = BookInfo.books1.filter(pk__lt=3)
     list = BookInfo.books1.filter(Q(pk__lt=3) | Q(bread__gt=2))
-
     context = {'list': list}
     return render(request, 'book/index.html', context)
+
+
+def postTest(request):
+    return render(request, 'book/postTest.html')
+
+
+def postTest1(request):
+    name = request.POST['uname']
+    pwd = request.POST['upwd']
+    gender = request.POST['ugender']
+    hobby = request.POST.getlist('uhobby')
+    return HttpResponse(request, 'book/postTest1.html')
